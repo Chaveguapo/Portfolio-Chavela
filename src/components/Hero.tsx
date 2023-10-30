9299a25dbfd5476964816e69208d9ec6c092db2d
Chaveguapo <catakeas@gmail.com>
2023-10-30 09:37:15 +0100

    Update hero and creation of content

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import "../App.css";
+
+export const Hero = () => {
+  return (
+    <>
+      <div className="container">
+        <header className="hero">
+          <div className="name">Chavela Guapo</div>
+          <div className="text-hero">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            deiusmod tempor incididunt ut labore.
+          </div>
+          <a href="#" className="button"></a>
+        </header>
+      </div>
+    </>
+  );
+};
